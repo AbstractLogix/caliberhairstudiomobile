b65f4c71ad861e2ec40b406b5e5b48f7555822ad
Oscar Miranda <abstractlogix@protonmail.com>
2021-09-20 21:37:08 -0400

    frontend

---
+++ frontend/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={require("/home/oscar/Dev/caliberhairstudio/frontend/assets/caliber_logo_150x150.png")} // must be a better way to write this
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
