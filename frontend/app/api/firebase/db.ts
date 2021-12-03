3d714e6e587bd681b1f95f5c43df0d8c97056e98
Brandon Behbahani <brandonbehbahani@gmail.com>
2021-12-03 15:10:34 -0500

    changes to firestore stuff
    I have added some functions to the db.ts that work well in console. These will be used as the database functions for the whole app. more testing is required.

---
+++ frontend/app/api/firebase/db.ts
@@ -0,0 +1,72 @@
+import { initializeApp } from "firebase/app";
+import {
+  getDoc,
+  getDocs, 
+  Firestore,
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  doc,
+  where,
+  serverTimestamp,} from 'firebase/firestore';
+// firebase config import
+import { firebaseConfig } from "./firebase_secrets";
+
+// initalize firebase
+const Firebase = initializeApp(firebaseConfig);
+
+const db = getFirestore(Firebase);
+
+// function to add artists to the collection of artists
+export async function addArtist(artistName: string, artistBio: string) {
+    // Add a new message entry to the Firebase database.
+    try {
+      await addDoc(collection(getFirestore(), 'artists'), {
+        name: artistName,
+        bio: artistBio,
+      });
+    }
+    catch(error) {
+      console.error('Error writing new message to Firebase Database', error);
+    }
+  }
+
+// Get a list of artists from firestore
+export async function getAllArtists(db: Firestore) {
+
+  const q = query(collection(db, "artists"), );
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+  return querySnapshot; 
+}
+
+// Get a single artist from firestore
+export async function getArtist(db: Firestore, artistID: string) {
+
+    const docRef = doc(db, "artists", artistID);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      return docSnap;
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+} 
+
+getAllArtists(db);
+
+
+
+    
