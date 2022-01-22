49e7d9cbb417e566d350d4fb83679be82183a9eb
Oscar Miranda <abstractlogix@protonmail.com>
2022-01-22 14:08:43 -0500

    added styles and components for future use

---
+++ src/app/components/index.tsx
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import RadioGroup from "./RadioGroup";
+import Dropdown from "./Dropdown";
+import TextInput from "./TextInput";
+import SegmentedControl from "./SegmentedControl";
+
+export { Button, RadioGroup, Dropdown, TextInput, SegmentedControl };
