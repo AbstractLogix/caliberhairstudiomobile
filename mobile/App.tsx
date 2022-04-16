c2acf0f3416f61c4f03623eb9f5307623eb479ee
Oscar Miranda <abstractlogix@protonmail.com>
2022-04-15 21:43:18 -0400

    Amplify initialization complete.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
 import NavigationStack from "./app/screens/NavigationStack";
+import React from "react";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return <NavigationStack></NavigationStack>;
