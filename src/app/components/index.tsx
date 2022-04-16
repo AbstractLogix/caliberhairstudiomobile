d229a510b3db3eb49044c2f8a535ba496f98333a
Oscar Miranda <abstractlogix@protonmail.com>
2022-04-15 21:12:34 -0400

    added aws amplify to project and reorganized

--- src/app/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-import RadioGroup from "./RadioGroup";
-import Dropdown from "./Dropdown";
-import TextInput from "./TextInput";
-import SegmentedControl from "./SegmentedControl";
-
-export { Button, RadioGroup, Dropdown, TextInput, SegmentedControl };
